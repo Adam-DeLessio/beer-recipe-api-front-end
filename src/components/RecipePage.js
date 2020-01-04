2fd68b9eea7866f73d7bd50332d3027654bce355
Adam DeLessio <adamdelessio@gmail.com>
2020-01-04 16:11:29 -0500

    styled ingredients

--- src/components/RecipePage.js
+++ src/components/RecipePage.js
@@ -11,7 +11,8 @@ class RecipePage extends Component {
 			recipe: '',
       malt: [],
       hops: [],
-      yeast: ''
+      yeast: '',
+      additional: ''
 		}
 	}
 
@@ -26,6 +27,7 @@ class RecipePage extends Component {
         this.setState({ malt: res[0].ingredients.malt })
         this.setState({ hops: res[0].ingredients.hops })
         this.setState({ yeast: res[0].ingredients.yeast })
+        this.setState({ additional: res[0].method.twist })
       })
       .catch(err => {
         console.error(err)
@@ -34,13 +36,39 @@ class RecipePage extends Component {
 
 
   render() {
+    let malts = this.state.malt.map(malt => {
+      return(
+        <li key={malt._id}>{malt.name}: {malt.amount.value} {malt.amount.unit}</li>
+      )
+    })
+    let hops = this.state.hops.map(hops => {
+      return(
+        <li key={hops._id}>{hops.name}: {hops.amount.value} {hops.amount.unit}</li>
+      )
+    })
     return(
       <div>
         <h1>{this.state.recipe.name} <span className='tagline'>({this.state.recipe.tagline})</span></h1>
         <h3>Brewer's Tips</h3>
         <p>{this.state.recipe.brewers_tips}</p>
-        <h3>Ingredients</h3>
-        <div></div>
+        <div className='ingredients'>
+          <h3>Malts</h3>
+          <div>
+            <ul>{malts}</ul>
+          </div>
+          <h3>Hops</h3>
+          <div>
+            <ul>{hops}</ul>
+          </div>
+          <h3>Yeast</h3>
+          <p>{this.state.yeast}</p>
+          <h3>Additional ingredients</h3>
+          <p>{this.state.additional}</p>
+        </div>
+        <div className='process'>
+
+        </div>
+
       </div>
     )
   }
